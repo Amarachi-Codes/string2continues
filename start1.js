709cb588e7ebeb4078332d260bf97cd25ebb740e
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2023-11-26 21:53:48 +0100

    updated

--- start1.js
+++ start1.js
@@ -6,3 +6,19 @@ Examples
 start1([1, 2, 3], [1, 3]) → 2
 start1([7, 2, 3], [1]) → 1
 start1([1, 2], []) → 1
+
+function start1(a, b) {
+    let len = 0;
+
+    // Check if the first element of array 'a' is 1
+    if (a.length > 0 && a[0] === 1) {
+        len++;
+    }
+
+    // Check if the first element of array 'b' is 1
+    if (b.length > 0 && b[0] === 1) {
+        len++;
+    }
+
+    return len;
+}
